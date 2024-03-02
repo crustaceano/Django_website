94f58456b53fe0b53e7413b20538b2b02d72052e
Омар Ахмедов <omar.axmedov.07@mail.ru>
2024-03-02 20:01:20 +0400

    change footer and header and add some bootstap stuff

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -129,10 +129,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# STATICFILES_FINDERS = (
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     # "compressor.finders.CompressorFinder",
+# )
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static_dev',
+    BASE_DIR / 'static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
