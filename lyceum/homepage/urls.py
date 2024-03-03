9a63b0dc9402386b409d8191a635d7713031cf3f
Омар Ахмедов <omar.axmedov.07@mail.ru>
2024-03-03 10:16:30 +0400

    change header and add about/description.html and add linkable header

--- lyceum/homepage/urls.py
+++ lyceum/homepage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+app_name = 'homepage'
 urlpatterns = [
     path('', views.home, name='homepage_main'),
     path('cofee/', views.cofee, name='homepage_cofeepoint'),
